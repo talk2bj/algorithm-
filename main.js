df41fd09a676a4bcd31de3197f539feddc701ba0
talk2bj <77057307+talk2bj@users.noreply.github.com>
2021-05-21 19:39:16 +0100

    assesment

---
+++ main.js
@@ -0,0 +1,13 @@
+//declare the value of kevin
+const kevin = 293;
+//converting to celsius
+const celsius = kevin - 273;
+// to estimate the value of celsius
+let fahrenheit = celsius * (9/5) + 32;
+//calculates fahrenheit figure
+fahrenheit = Math.floor(fahrenheit);
+console.log(`The temperature is ${fahrenheit}degrees Fahrenheit`);
+
+let Newton = celsius *(33/100);
+Newton = Math.floor(Newton);
+console.log(`The temperature is ${Newton} degrees Newton`);
